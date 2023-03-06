db9574d5adce4ff23230efcb9fc0d6a89dfe9909
Eliot Gronstal <eliot.lauren@gmail.com>
2023-03-06 15:03:05 -0800

    update Views>Items>Create with html helpers.

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -1,67 +1,36 @@
 using Microsoft.AspNetCore.Mvc;
 using ToDoList.Models;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ToDoList.Controllers
 {
   public class ItemsController : Controller
   {
-    // [HttpGet("/items")]
-    // public ActionResult Index()
-    // {
-    //   List<Item> allItems = Item.GetAll();
-    //   return View(allItems);
-    // }
+    private readonly ToDoListContext _db;
 
-    [HttpGet("/items/new")]
-    public ActionResult CreateForm()
+    public ItemsController(ToDoListContext db)
     {
-      return View();
+      _db = db;
     }
 
-    [HttpPost("/items")]
-    public ActionResult Create(string description)
+    public ActionResult Index()
     {
-      Item myItem = new Item(description, 1);
-      return RedirectToAction("Index");
-    }
-
-    // [HttpPost("/items/delete")]
-    // public ActionResult DeleteAll()
-    // {
-    //   Item.ClearAll();
-    //   return View();
-    // }
-
-    [HttpGet("/items/new")]
-    public ActionResult New()
-    {
-      return View();
-    }
-
-    [HttpGet("/items/{id}")]
-    public ActionResult Show(int id)
-    {
-      Item foundItem = Item.Find(id);
-      return View(foundItem);
-    }
-
-    [HttpGet("/categories/{categoryId}/items/{itemId}")]
-    public ActionResult Show(int categoryId, int itemId)
-    {
-      Item item = Item.Find(itemId);
-      Category category = Category.Find(categoryId);
-      Dictionary<string, object> model = new Dictionary<string, object>();
-      model.Add("item", item);
-      model.Add("category", category);
+      List<Item> model = _db.Items.ToList();
       return View(model);
     }
 
-    [HttpGet("/categories/{categoryId}/items/new")]
-    public ActionResult New(int categoryId)
+    public ActionResult Create()
     {
-      Category category = Category.Find(categoryId);
-      return View(category);
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Item item)
+    {
+      _db.Items.Add(item);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
     }
   }
 }
